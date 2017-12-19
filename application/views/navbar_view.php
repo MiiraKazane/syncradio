281a00c95a9b973b14699b51fe45c93487272a77
MiiraKazane <miofher@gmail.com>
2017-12-18 22:17:16 -0700

    borrar comentrios

--- application/views/navbar_view.php
+++ application/views/navbar_view.php
@@ -16,21 +16,15 @@
 <div class="wrapper">
     <header class="main-header">
 
-    <!-- Logo -->
     <a href="<?= (SITE . 'login'); ?>" class="logo">
-      <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini">SSR</span>
-      <!-- logo for regular state and mobile devices -->
       <span class="logo-lg">SyncRadio</span>
     </a>
 
-    <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
-      <!-- Sidebar toggle button-->
       <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
         <span class="sr-only">Toggle navigation</span>
       </a>
-      <!-- Navbar Right Menu -->
       <div class="navbar-custom-menu">
         <ul class="nav navbar-nav">
 
@@ -72,8 +66,6 @@
 
     </nav>
   </header>
-
-  <!-- Left side column. contains the logo and sidebar -->
    <aside class="main-sidebar">
     <section class="sidebar">
       <ul class="sidebar-menu" data-widget="tree">
